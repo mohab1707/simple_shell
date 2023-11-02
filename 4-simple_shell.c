20705bb6fa8c96157c49fcf07b3ee15d73d8fe20
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-11-02 04:49:32 -0700

    4

---
+++ 4-simple_shell.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define BUFFER_SIZE 1024
+
+int main(void)
+{
+char buffer[BUFFER_SIZE];
+char *args[BUFFER_SIZE];
+int i;
+int status;
+pid_t pid;
+char *path;
+char *path_copy;
+char *token;
+char *full_path;
+int command_found;
+while (1)
+{
+printf(":) ");
+if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+{
+break;
+}
+buffer[strcspn(buffer, "\n")] = '\0';
+args[0] = strtok(buffer, " ");
+i = 0;
+while (args[i] != NULL)
+{
+i++;
+args[i] = strtok(NULL, " ");
+}
+path = getenv("PATH");
+path_copy = strdup(path);
+token = strtok(path_copy, ":");
+full_path = NULL;
+command_found = 0;
+while (token != NULL)
+{
+full_path = malloc(strlen(token) + strlen(args[0]) + 2);
+if (full_path == NULL)
+{
+perror("Memory allocation failed");
+exit(EXIT_FAILURE);
+}
+sprintf(full_path, "%s/%s", token, args[0]);
+if (access(full_path, F_OK) != -1)
+{
+command_found = 1;
+break;
+}
+free(full_path);
+token = strtok(NULL, ":");
+}
+free(path_copy);
+if (strcmp(args[0], "exit") == 0)
+{
+exit(0);
+}
+if (command_found)
+{
+pid = fork();
+if (pid == -1)
+{
+perror("Fork failed");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+if (execve(full_path, args, NULL) == -1)
+{
+perror("Execve failed");
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+wait(&status);
+if (WIFEXITED(status) && WEXITSTATUS(status) == 127)
+{
+printf("%s: not found\n", args[0]);
+}
+}
+}
+else
+{
+printf("%s: not found\n", args[0]);
+}
+}
+return (0);
+}
