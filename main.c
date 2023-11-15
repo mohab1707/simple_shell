5691986d12b04e2855f61a1f1ca53656010e33ea
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-11-15 11:04:55 -0800

    task2

--- main.c
+++ main.c
@@ -19,7 +19,8 @@ int main(void)
 char buffer[BUFFER_SIZE];
 int status;
 pid_t pid;
-char *args[2];
+int i;
+ char *args[BUFFER_SIZE];
 while (1)
 {
 printf("#cisfun$ ");
@@ -28,6 +29,13 @@ if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
 break;
 }
 buffer[strcspn(buffer, "\n")] = '\0';
+args[0] = strtok(buffer, " ");
+i = 0;
+while (args[i] != NULL)
+{
+i++;
+args[i] = strtok(NULL, " ");
+}
 pid = fork();
 if (pid == -1)
 {
@@ -36,9 +44,7 @@ exit(EXIT_FAILURE);
 }
 else if (pid == 0)
 {
-args[0] = buffer;
-args[1] = NULL;
-if (execve(buffer, args, NULL) == -1)
+if (execve(args[0], args, NULL) == -1)
 {
 perror("./shell");
 exit(EXIT_FAILURE);
@@ -49,7 +55,7 @@ else
 wait(&status);
 if (WIFEXITED(status) && WEXITSTATUS(status) == 127)
 {
-printf("%s: not found\n", buffer);
+printf("%s: not found\n", args[0]);
 }
 }
 }
