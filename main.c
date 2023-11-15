33209c76bd44893d9347dcbe6e0df1e43984d4aa
Mohamed ABABSA <m.ababsa@esi-sba.dz>
2023-11-15 11:18:38 -0800

    task3

--- main.c
+++ main.c
@@ -17,13 +17,18 @@
 int main(void)
 {
 char buffer[BUFFER_SIZE];
+char *args[BUFFER_SIZE];
+int i;
 int status;
 pid_t pid;
-int i;
- char *args[BUFFER_SIZE];
+char *path;
+char *path_copy;
+char *token;
+char *full_path;
+int command_found;
 while (1)
 {
-printf("#cisfun$ ");
+printf(":) ");
 if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
 {
 break;
@@ -36,6 +41,31 @@ while (args[i] != NULL)
 i++;
 args[i] = strtok(NULL, " ");
 }
+path = getenv("PATH");
+path_copy = strdup(path);
+token = strtok(path_copy, ":");
+command_found = 0;
+while (token != NULL)
+{
+full_path = malloc(strlen(token) + strlen(args[0]) + 2);
+if (full_path == NULL)
+{
+perror("Memory allocation failed");
+exit(EXIT_FAILURE);
+}
+sprintf(full_path, "%s/%s", token, args[0]);
+if (access(full_path, F_OK) != -1)
+{
+command_found = 1;
+break;
+}
+free(full_path);
+token = strtok(NULL, ":");
+}
+free(path_copy);
+if (command_found)
+{
+printf("Executing: %s\n", full_path);
 pid = fork();
 if (pid == -1)
 {
@@ -44,9 +74,9 @@ exit(EXIT_FAILURE);
 }
 else if (pid == 0)
 {
-if (execve(args[0], args, NULL) == -1)
+if (execve(full_path, args, NULL) == -1)
 {
-perror("./shell");
+perror("Execve failed");
 exit(EXIT_FAILURE);
 }
 }
@@ -57,6 +87,12 @@ if (WIFEXITED(status) && WEXITSTATUS(status) == 127)
 {
 printf("%s: not found\n", args[0]);
 }
+}
+free(full_path);
+}
+else
+{
+printf("%s: not found\n", args[0]);
 }
 }
 return (0);
